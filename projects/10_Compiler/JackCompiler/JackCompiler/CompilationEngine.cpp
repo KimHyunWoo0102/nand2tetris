e40069112772be90906afb1a561c3ef2428b39e6
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-10-15 13:51:07 +0900

    Feat(Compilation Engine) : CompilationEngine의 기본 구조 및 compileClass 1차 구현
    Jack 파서의 핵심인 CompilationEngine의 초기 구조를 구현함.
    
    - public API인 `compile()`과 private 진입점인 `compileClass()`를 포함한 기본 골격 완성.
    - Jack 문법 규칙에 따른 모든 private `compile...` 헬퍼 메서드들의 뼈대를 선언함.
    - Xml 파일을 작성하는 데 필요한 헬퍼 메서드인 process와 escapeXml
      정의함.
    - `compileClass` 메서드에 클래스의 최상위 구조(`class`, `className`, `{`, `}`)를 처리하는 기본 로직을 1차적으로 구현 완료함.

---
+++ projects/10_Compiler/JackCompiler/JackCompiler/CompilationEngine.cpp
@@ -0,0 +1,103 @@
+#include "CompilationEngine.h"
+
+//----------------------------------------------------
+// constructor
+//----------------------------------------------------
+
+CompilationEngine::CompilationEngine(Tokenizer& tokenizer, const std::string& outputFilename)
+	:tokenizer(tokenizer),ofs(outputFilename)
+{
+	if (!ofs.is_open()) {
+		throw std::runtime_error("CompilationEngine() : can't open write file : " + outputFilename);
+	}
+}
+
+//-----------------------------------------------------
+// public method
+//-----------------------------------------------------
+
+void CompilationEngine::compile()
+{
+	this->tokenizer.advance();
+	this->compileClass();
+}
+
+//-----------------------------------------------------
+// private helper method
+//-----------------------------------------------------
+
+std::string CompilationEngine::escapeXml(char symbol) {
+    switch (symbol) {
+    case '<':  return "&lt;";
+    case '>':  return "&gt;";
+    case '&':  return "&amp;";
+    case '"':  return "&quot;";
+    default:   return std::string(1, symbol);
+    }
+}
+
+//-----------------------------------------------------
+
+void CompilationEngine::process(Token::KeywordType expectedKeyword)
+{
+    if (tokenizer.tokenType() == Token::TokenType::KEYWORD && tokenizer.keyword() == expectedKeyword) {
+        ofs << "<keyword> " << keywordToString(expectedKeyword) << " </keyword>\n";
+        tokenizer.advance();
+    }
+    else {
+        std::string got = (tokenizer.tokenType() == Token::TokenType::KEYWORD)
+            ? keywordToString(tokenizer.keyword())
+            : "a non-keyword token";
+        throw std::runtime_error("Syntax Error: Expected keyword '" + keywordToString(expectedKeyword) + "', but got '" + got + "'");
+    }
+}
+
+//-----------------------------------------------------
+
+void CompilationEngine::process(char expectedSymbol) {
+    if (tokenizer.tokenType() == Token::TokenType::SYMBOL && tokenizer.symbol() == expectedSymbol) {
+        // 이제 내부 헬퍼 함수를 호출한다.
+        ofs << "<symbol> " << escapeXml(expectedSymbol) << " </symbol>\n";
+        tokenizer.advance();
+    }
+    else {
+        throw std::runtime_error("Syntax Error: Expected a symbol + " + escapeXml(expectedSymbol) + ". got = " + escapeXml(tokenizer.symbol()));
+    }
+}
+
+//-----------------------------------------------------
+
+void CompilationEngine::process(Token::TokenType expectedType)
+{
+    if (tokenizer.tokenType() == expectedType) {
+        switch (expectedType) {
+        case Token::TokenType::IDENTIFIER:
+            ofs << "<identifier> " << tokenizer.identifier() << " </identifier>\n";
+            break;
+        case Token::TokenType::INT_CONST:
+            ofs << "<integerConstant> " << tokenizer.intVal() << " </integerConstant>\n";
+            break;
+        case Token::TokenType::STRING_CONST:
+            ofs << "<stringConstant> " << tokenizer.stringVal() << " </stringConstant>\n";
+            break;
+        default:
+            throw std::logic_error("process(TokenType) called with an unhandled type.");
+        }
+        tokenizer.advance();
+    }
+    else {
+        throw std::runtime_error("Syntax Error: Expected a token of type " + Token::tokenTypeToString(expectedType) +
+            ", but found a different type.");
+    }
+}
+//-----------------------------------------------------
+
+void CompilationEngine::compileClass()
+{
+    ofs << "<Class>\n";
+    process(Token::KeywordType::CLASS);
+    process(Token::TokenType::IDENTIFIER);
+    process('{');
+    // TODO : class field, static, subroutine compile
+    process('}');
+}
